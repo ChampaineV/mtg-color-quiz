826a0736974c2ba6e5607b45aefe89d120fd0919
Leischow Vang <champainevaj@gmail.com>
2024-04-16 14:22:26 -0500

    Rename Welcome to Home. Organized imports

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
-function Welcome() {
+function Home() {
 
     return (
-      <div className="Welcome">
-        <header className="Welcome-header">
+      <div className="Home">
+        <header className="Home-header">
           <h1>Magic The Gathering Color Quiz</h1>
           <p>
           Welcome! This is a Magic The Gathering Color quiz made with HTML, CSS, and JavaScript. Users will be able to answer the following questions that will determine what color is best suited to their play style. Though this may not be 100% accurate, please enjoy!
