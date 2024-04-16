94676fdc385a0bc0d2551601ab90d6bfceb23706
Leischow Vang <champainevaj@gmail.com>
2024-04-16 14:36:11 -0500

    Troubleshoot Attempted import error issue

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Magic The Gathering Color Quiz</h1>
+          <p>
+          Welcome! This is a Magic The Gathering Color quiz made with HTML, CSS, and JavaScript. Users will be able to answer the following questions that will determine what color is best suited to their play style. Though this may not be 100% accurate, please enjoy!
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
