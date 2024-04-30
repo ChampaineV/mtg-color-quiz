3329731184654dbfda6e76a1d6e6ebccaaab36c2
Leischow Vang <champainevaj@gmail.com>
2024-04-30 13:28:26 -0500

    Removed Quiz component from App.js for the time being

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
           <p>
           Welcome! This is a Magic The Gathering Color quiz made with HTML, CSS, and JavaScript. Users will be able to answer the following questions that will determine what color is best suited to their play style. Though this may not be 100% accurate, please enjoy!
           </p>
-          <a><Quiz /></a>
       </header>
     </div>
   );
