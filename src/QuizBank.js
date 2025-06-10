61dae56e59dd2a1bd37e72acb5ba078395a7e7ce
ChampaineV <champainevaj@gmail.com>
2025-06-10 10:16:47 -0500

    Add more questions to QuizBank

--- src/QuizBank.js
+++ src/QuizBank.js
@@ -1,7 +1,7 @@
 const colorNames = ['Green', 'Red', 'Black', 'Blue', 'White'];
 
 const QuizBank = {
-    totalQuestions: 7,
+    totalQuestions: 4,
     colorNames, // Export color names for use in results
     questions : [
     {
@@ -15,24 +15,29 @@ const QuizBank = {
         answers : [2, 1, 3, 4, 0]
     },
     {
-        question : 'In a group project, what role do you play??',
+        question : 'In a group project, what role do you play?',
         choices : ['I am a Leader. I make sure my team has a clear objective and have a good sense of when a problem is solved', 'I am a Doer. I know where to go to find information and I am proactive about doing so', 'I am a Challenger. I can look at problems and see more than one solution to drive effectiveness.', 'I am a Thinker. I share ideas and break down information for the best solution.', 'I am a Supporter. I want to make sure everyone in my team is heard.'],
         answers : [0, 1, 2, 3, 4]
     },
     {
-        question : 'What is your favorite season?',
-        choices : ['Spring', 'Summer', 'Autumn', 'Winter', 'Monsoon'],
-        answers : [0, 1, 2, 3, 4]
+        question : 'What do you dislike the most?',
+        choices : ['I hate when others are selfish/reckless', 'I hate when plans change abruptly', 'I hate seeing people with a \'savior\' complex', 'I cannot stand being bored/frustrated', 'I hate when people can\'t move away from tradition'],
+        answers : [4, 0, 2, 1, 3]
     },
     {
-        question : 'What is your favorite animal?',
-        choices : ['Dog', 'Cat', 'Bird', 'Fish', 'Reptile'],
+        question : 'What trait are you best known for?',
+        choices : ['My kindness', 'My intellect', 'My wisdom', 'My humor', 'My persistence'],
         answers : [4, 3, 0, 1, 2]
-    },
+    }/*,
     {
         question : 'What is your favorite food?',
         choices : ['Pizza', 'Burger', 'Sushi', 'Salad', 'Pasta'],
         answers : [1, 2, 4, 0, 3]
-    }
+    },
+    {
+        question : 'What is your favorite season?',
+        choices : ['Spring', 'Summer', 'Fall', 'Winter', 'Monsoon'],
+        answers : [0, 1, 2, 3, 4]
+    }*/
     ]
 }
