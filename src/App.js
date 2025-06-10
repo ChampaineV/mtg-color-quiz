3c41c597e6352e7dd2bd56b2ac3a6f6461ddefd7
ChampaineV <champainevaj@gmail.com>
2025-06-10 09:54:10 -0500

    Reworked App to include Next button and Quiz

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
 //Handles logic for scoring and questions is written and other components are imported
 import './App.css';
 import Quiz from './Quiz';
+import { useState } from 'react';
 
 function App() {
+  // State to track if the user has started the quiz
+  const [started, setStarted] = useState(false);
+
+  // Show welcome screen with Next button, then show Quiz when started
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Magic The Gathering Color Quiz</h1>
-          <p>
+        <h1>Magic The Gathering Color Quiz</h1>
+        <p>
           Welcome! This is a Magic The Gathering Color quiz made with HTML, CSS, and JavaScript. Users will be able to answer the following questions that will determine what color is best suited to their play style. Though this may not be 100% accurate, please enjoy!
-          </p>
+        </p>
+        {!started ? (
+          <button onClick={() => setStarted(true)}>
+            Next
+          </button>
+        ) : (
+          <Quiz />
+        )}
       </header>
     </div>
   );
-
-  function matchAndAddValue(userAnswer, choices, answers, colorProfiles) {
-    // Find the index where the userAnswer matches in choices
-    const indexInChoices = choices.indexOf(userAnswer);
-
-    // If the userAnswer is found in choices
-    if (indexInChoices !== -1) {
-        // Get the corresponding answer value from answers
-        const answerValue = answers[indexInChoices];
-
-        // Find the index where the answer value matches in colorProfiles
-        const indexInColorProfiles = Object.keys(colorProfiles).indexOf(answerValue);
-
-        // If the answer value is found in colorProfiles
-        if (indexInColorProfiles !== -1) {
-            // Increment the count for the corresponding color profile
-            const color = Object.keys(colorProfiles)[indexInColorProfiles];
-            colorProfiles[color]++;
-        }
-    }
-}
 }
 
 export default App;
