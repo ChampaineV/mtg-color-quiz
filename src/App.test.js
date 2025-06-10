f0165fcd5effcdb89548240c64f1c5c17f2cd284
ChampaineV <champainevaj@gmail.com>
2025-06-10 10:32:18 -0500

    Retrying app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders quiz title', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const titleElement = screen.getByText(/Magic The Gathering Color Quiz/i);
-  expect(titleElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
